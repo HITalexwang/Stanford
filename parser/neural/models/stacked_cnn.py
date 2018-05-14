8a2ab1a62f474dac88efce2d0ae8e293a5edb74a
HITalexwang <hitalexwang@gmail.com>
2018-05-13 20:41:26 -0700

    split gate matrix from conv, not compatible anymore with previous gated conv_func model

--- parser/neural/models/stacked_cnn.py
+++ parser/neural/models/stacked_cnn.py
@@ -71,14 +71,15 @@ class StackedCNN(NN):
       output_size = self.mlp_size
     
     if self.conv_func.__name__.startswith('gated'):
-      output_size *= 2
-      #gate = self.convolutional(inputs, window_size, output_size, dilation=dilation, keep_prob=keep_prob, n_splits=n_splits, add_bias=add_bias, initializer=None)
+      #output_size *= 2
+      with tf.variable_scope('Gate'):
+        gate = self.convolutional(inputs, window_size, output_size, dilation=dilation, keep_prob=keep_prob, n_splits=n_splits, add_bias=add_bias, initializer=None)
     convolutional = self.convolutional(inputs, window_size, output_size, dilation=dilation, keep_prob=keep_prob, n_splits=n_splits, add_bias=add_bias, initializer=None)
     
-    #if self.conv_func.__name__.startswith('gated'):
-      #return self.conv_func([convolutional, gate])
-    #else:
-    return self.conv_func(convolutional)
+    if self.conv_func.__name__.startswith('gated'):
+      return self.conv_func([convolutional, gate])
+    else:
+      return self.conv_func(convolutional)
 
   #=============================================================
   def convolutional(self, inputs, window_size, output_size, dilation=1, keep_prob=None, n_splits=1, add_bias=True, initializer=None):
