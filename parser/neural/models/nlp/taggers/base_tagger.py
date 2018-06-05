cc407a5ca7dc3012f9cd707de36c07f3ebf0cc18
HITalexwang <hitalexwang@gmail.com>
2018-06-05 08:14:40 -0700

    add xpos tagger

--- parser/neural/models/nlp/taggers/base_tagger.py
+++ parser/neural/models/nlp/taggers/base_tagger.py
@@ -68,13 +68,21 @@ class BaseTagger(NN):
   #=============================================================
   def process_accumulators(self, accumulators, time=None):
     """"""
-    
-    n_tokens, n_seqs, loss, corr, seq_corr = accumulators
+    #'n_tokens', 'n_seqs', 'loss', 'n_tags_correct', 'n_tags_seqs_correct', 'n_xtags_correct', 'n_xtags_seqs_correct'
+    if 'xtags' in self.output_vocabs:
+      n_tokens, n_seqs, loss, tags_corr, tags_seq_corr, xtags_corr, xtags_seq_corr = accumulators
+    else:
+      n_tokens, n_seqs, loss, tags_corr, tags_seq_corr = accumulators
     acc_dict = {
       'Loss': loss,
-      'TS': corr/n_tokens*100,
-      'SS': seq_corr/n_seqs*100,
+      'Tags-TS': tags_corr/n_tokens*100,
+      'Tags-SS': tags_seq_corr/n_seqs*100,
     }
+    if 'xtags' in self.output_vocabs:
+      acc_dict.update({
+        'XTags-TS': xtags_corr/n_tokens*100,
+        'XTags-SS': xtags_seq_corr/n_seqs*100,
+      })
     if time is not None:
       acc_dict.update({
         'Token_rate': n_tokens / time,
@@ -89,7 +97,7 @@ class BaseTagger(NN):
     acc_dict = self.process_accumulators(accumulators)
     for key, value in acc_dict.iteritems():
       history[key].append(value)
-    return history['TS'][-1]
+    return history['Tags-TS'][-1]
   
   #=============================================================
   def print_accuracy(self, accumulators, time, prefix='Train'):
@@ -98,8 +106,12 @@ class BaseTagger(NN):
     acc_dict = self.process_accumulators(accumulators, time=time)
     strings = []
     strings.append('Loss: {Loss:7.3f}')
-    strings.append('TS: {TS:5.2f}%')
-    strings.append('SS: {SS:5.2f}%')
+    strings.append('TS: Tags: {Tags-TS:5.2f}%')
+    if 'xtags' in self.output_vocabs:
+      strings.append('XTags: {XTags-TS:5.2f}%')
+    strings.append('SS: Tags: {Tags-SS:5.2f}%')
+    if 'xtags' in self.output_vocabs:
+      strings.append('XTags: {XTags-SS:5.2f}%')
     strings.append('Speed: {Seq_rate:6.1f} seqs/sec')
     string = '{0}  ' + ' | '.join(strings)
     print(string.format(prefix, **acc_dict))
@@ -114,12 +126,19 @@ class BaseTagger(NN):
   #=============================================================
   def check(self, preds, sents, fileobj):
     """"""
-
-    for tokens, preds in zip(sents, preds[0]):
-      for token, pred in zip(zip(*tokens), preds):
-        tag = self.vocabs['tags'][pred]
-        fileobj.write('\t'.join(token+(tag, ))+'\n')
-      fileobj.write('\n')
+    if 'xtags' in self.output_vocabs:
+      for tokens, tags_preds, xtags_preds in zip(sents, preds[0], preds[1]):
+        for token, tags_pred, xtags_pred in zip(zip(*tokens), tags_preds, xtags_preds):
+          tag = self.vocabs['tags'][tags_pred]
+          xtag = self.vocabs['xtags'][xtags_pred]
+          fileobj.write('\t'.join(token+(tag, xtag))+'\n')
+        fileobj.write('\n')
+    else:
+      for tokens, preds in zip(sents, preds[0]):
+        for token, pred in zip(zip(*tokens), preds):
+          tag = self.vocabs['tags'][pred]
+          fileobj.write('\t'.join(token+(tag, ))+'\n')
+        fileobj.write('\n')
     return
 
   #=============================================================
@@ -129,6 +148,8 @@ class BaseTagger(NN):
     # Turns list of tuples of tensors into list of matrices
     tag_probs = [tag_prob for batch in probs for tag_prob in batch[0]]
     tokens_to_keep = [weight for batch in probs for weight in batch[1]]
+    if 'xtags' in self.output_vocabs:
+      xtag_probs = [xtag_prob for batch in probs for xtag_prob in batch[2]]
     tokens = [sent for batch in sents for sent in batch]
     j = 0
     with codecs.open(output_file, 'w', encoding='utf-8', errors='ignore') as f:
@@ -136,8 +157,12 @@ class BaseTagger(NN):
         sent, tag_prob, weights = tokens[i], tag_probs[i], tokens_to_keep[i]
         sent = zip(*sent)
         tag_preds = np.argmax(tag_prob, axis=1)
+        xtag_preds = np.zeros(len(tag_preds))
+        if 'xtags' in self.output_vocabs:
+          xtag_prob = xtag_probs[i]
+          xtag_preds = np.argmax(xtag_prob, axis=1)
         merge_line = merge_lines[j]
-        for token, tag_pred, weight in zip(sent, tag_preds[1:], weights[1:]):
+        for token, tag_pred, xtag_pred, weight in zip(sent, tag_preds[1:], xtag_preds[1:], weights[1:]):
           token = list(token)
           if (int(token[0]) in merge_line.keys()):
             f.write(merge_line[int(token[0])]+'\n')
@@ -145,6 +170,8 @@ class BaseTagger(NN):
           token.append('_')
           token.append('_')
           token[3] = self.vocabs['tags'][tag_pred]
+          if 'xtags' in self.output_vocabs:
+            token[4] = self.vocabs['xtags'][xtag_pred]
           f.write('\t'.join(token)+'\n')
         j += 1
         if j < len(inv_idxs):
@@ -154,14 +181,23 @@ class BaseTagger(NN):
   #=============================================================
   @property
   def train_keys(self):
-    return ('n_tokens', 'n_seqs', 'loss', 'n_correct', 'n_seqs_correct')
+    if 'xtags' in self.output_vocabs:
+      return ('n_tokens', 'n_seqs', 'loss', 'n_tags_correct', 'n_tags_seqs_correct', 'n_xtags_correct', 'n_xtags_seqs_correct')
+    else:
+      return ('n_tokens', 'n_seqs', 'loss', 'n_tags_correct', 'n_tags_seqs_correct')
   
   #=============================================================
   @property
   def valid_keys(self):
-    return ('preds', )
+    if 'xtags' in self.output_vocabs:
+      return ('tags_preds', 'xtags_preds')
+    else:
+      return ('tags_preds')
 
   #=============================================================
   @property
   def parse_keys(self):
-    return ('probs', 'tokens_to_keep')
+    if 'xtags' in self.output_vocabs:
+      return ('tags_probs', 'tokens_to_keep', 'xtags_probs')
+    else:
+      return ('tags_probs', 'tokens_to_keep')
--- parser/neural/models/nlp/taggers/tagger.py
+++ parser/neural/models/nlp/taggers/tagger.py
@@ -36,35 +36,56 @@ class Tagger(BaseTagger):
     int_tokens_to_keep = tf.to_int32(self.tokens_to_keep)
     
     with tf.variable_scope('MLP'):
-      mlp = self.MLP(top_recur, self.mlp_size)
+      with tf.variable_scope('Tag'):
+        tags_mlp = self.MLP(top_recur, self.mlp_size)
+      if 'xtags' in self.output_vocabs:
+        with tf.variable_scope('XTag'):
+          xtags_mlp = self.MLP(top_recur, self.mlp_size)
     
     with tf.variable_scope('Tag'):
-      logits = self.linear(mlp, len(self.vocabs['tags']))
-      probs = tf.nn.softmax(logits)
-      preds = tf.to_int32(tf.argmax(logits, axis=-1))
-      targets = self.vocabs['tags'].placeholder
-      correct = tf.to_int32(tf.equal(preds, targets))*int_tokens_to_keep
-      loss = tf.losses.sparse_softmax_cross_entropy(targets, logits, self.tokens_to_keep)
+      tags_logits = self.linear(tags_mlp, len(self.vocabs['tags']))
+      tags_probs = tf.nn.softmax(tags_logits)
+      tags_preds = tf.to_int32(tf.argmax(tags_logits, axis=-1))
+      tags_targets = self.vocabs['tags'].placeholder
+      tags_correct = tf.to_int32(tf.equal(tags_preds, tags_targets))*int_tokens_to_keep
+      tags_loss = tf.losses.sparse_softmax_cross_entropy(tags_targets, tags_logits, self.tokens_to_keep)
+      loss = tags_loss
+    if 'xtags' in self.output_vocabs:
+      with tf.variable_scope('XTag'):
+        xtags_logits = self.linear(xtags_mlp, len(self.vocabs['xtags']))
+        xtags_probs = tf.nn.softmax(xtags_logits)
+        xtags_preds = tf.to_int32(tf.argmax(xtags_logits, axis=-1))
+        xtags_targets = self.vocabs['xtags'].placeholder
+        xtags_correct = tf.to_int32(tf.equal(xtags_preds, xtags_targets))*int_tokens_to_keep
+        xtags_loss = tf.losses.sparse_softmax_cross_entropy(xtags_targets, xtags_logits, self.tokens_to_keep)
+        loss += xtags_loss
     
-    
-    n_correct = tf.reduce_sum(correct)
-    n_seqs_correct = tf.reduce_sum(tf.to_int32(tf.equal(tf.reduce_sum(correct, axis=1), self.sequence_lengths-1)))
+    n_tags_correct = tf.reduce_sum(tags_correct)
+    n_tags_seqs_correct = tf.reduce_sum(tf.to_int32(tf.equal(tf.reduce_sum(tags_correct, axis=1), self.sequence_lengths-1)))
+    n_xtags_correct = tf.reduce_sum(xtags_correct)
+    n_xtags_seqs_correct = tf.reduce_sum(tf.to_int32(tf.equal(tf.reduce_sum(xtags_correct, axis=1), self.sequence_lengths-1)))
     
     outputs = {
-      'logits': logits,
-      'probs': probs,
-      'preds': preds,
-      'targets': targets,
-      'correct': correct,
-      'loss': loss,
-      'n_correct': n_correct,
+      'tags_logits': tags_logits,
+      'tags_probs': tags_probs,
+      'tags_preds': tags_preds,
+      'tags_targets': tags_targets,
+      'tags_correct': tags_correct,
+      'n_tags_correct': n_tags_correct,
+      'n_tags_seqs_correct': n_tags_seqs_correct,
+
+      'xtags_logits': xtags_logits,
+      'xtags_probs': xtags_probs,
+      'xtags_preds': xtags_preds,
+      'xtags_targets': xtags_targets,
+      'xtags_correct': xtags_correct,
+      'n_xtags_correct': n_xtags_correct,
+      'n_xtags_seqs_correct': n_xtags_seqs_correct,
       
+      'loss': loss,
       'n_tokens': self.n_tokens,
       'n_seqs': self.batch_size,
-      'tokens_to_keep': self.tokens_to_keep,
-      'n_correct': n_correct,
-      'n_seqs_correct': n_seqs_correct,
-      'loss': loss
+      'tokens_to_keep': self.tokens_to_keep
     }
     
     return outputs
